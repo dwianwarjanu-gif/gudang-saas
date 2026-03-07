ba0c262575aa15ffcc78adfe26fab0cdb433b5bc
Dwi Janu Anwar <dwianwarjanu@gmail.com>
2026-03-07 16:51:15 +0700

    Add files via upload

---
+++ src/controllers/authController.js
@@ -0,0 +1,28 @@
+const prisma = require('../utils/prisma')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+exports.registerTenant = async (req, res) => {
+  const { email, password, tenantName, subdomain } = req.body
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const tenant = await prisma.tenant.create({
+    data: {
+      name: tenantName,
+      subdomain,
+      users: {
+        create: {
+          email,
+          password: hashedPassword,
+          role: "owner"
+        }
+      }
+    }
+  })
+
+  res.json({
+    message: "Tenant created",
+    tenant
+  })
+}
