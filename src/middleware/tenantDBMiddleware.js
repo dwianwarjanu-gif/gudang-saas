e101c9191a6fd7eb98466a14f3c215931827d90b
Dwi Janu Anwar <dwianwarjanu@gmail.com>
2026-03-09 14:08:34 +0700

    Refactor tenantDBMiddleware to use new connection config
    Updated tenantDBMiddleware to use tenantConnection config for database connections.

--- src/middleware/tenantDBMiddleware.js
+++ src/middleware/tenantDBMiddleware.js
@@ -1,4 +1,32 @@
 const getTenantConnection = require("../utils/tenantDB");
+const getTenantConnection = require("../config/tenantConnection");
+
+module.exports = async function tenantDBMiddleware(req, res, next) {
+
+ try {
+
+  if (!req.tenant) {
+   return res.status(400).json({ error: "Tenant not resolved" });
+  }
+
+  const db = await getTenantConnection(req.tenant.dbName);
+
+  req.db = db;
+
+  next();
+
+ } catch (error) {
+
+  console.error("Tenant DB connection error:", error);
+
+  res.status(500).json({
+   error: "Tenant database connection failed"
+  });
+
+ }
+
+};
+
 
 async function tenantDBMiddleware(req, res, next) {
 
