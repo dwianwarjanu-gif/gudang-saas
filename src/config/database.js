13bdecaac1752706a90764c35cf2733732120fa6
Dwi Janu Anwar <dwianwarjanu@gmail.com>
2026-03-12 09:14:24 +0700

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,11 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
  host: "localhost",
  user: "root",
- password: "",
+ password: "Trizlab@2026!",
  database: "saas_master",
  waitForConnections: true,
  connectionLimit: 10
 });
+
 
 module.exports = pool;
