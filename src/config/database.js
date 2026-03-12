15e92c8feea4a86dc6e33a7abe10ecbacc1f6164
Dwi Janu Anwar <dwianwarjanu@gmail.com>
2026-03-12 09:39:16 +0700

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2/promise");
 // Role Admin
 const pool = mysql.createPool({
  host: "localhost",
- user: "root",
- password: "Trizlab@2026!",
+ user: "saas_user",
+ password: "SaasPassword123!",
  database: "saas_master",
  waitForConnections: true,
  connectionLimit: 10
@@ -12,19 +12,7 @@ const pool = mysql.createPool({
 
 });
 
-
-// Role User
-const pool = mysql.createPool({
- host: "localhost",
- user: "saas_user",
- password: "SaasPassword123!",
- database: "saas_db",
- waitForConnections: true,
- connectionLimit: 10
- queueLimit: 0
-
-});
-
 module.exports = pool;
+
 
 
